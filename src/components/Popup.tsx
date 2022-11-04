e480faf818fe18ed3eabc89bbc6884d1f2530a86
Wide <khairadenisa@gmail.com>
2022-11-04 17:06:16 +0700

    Added youtube video for popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import YouTube from 'react-youtube';
+// @ts-ignore
+import movieTrailer from 'movie-trailer'
 
 interface PopupProps {
     popup: any,
@@ -7,21 +9,46 @@ interface PopupProps {
 }
 
 export const Popup: React.FC<PopupProps> = ({popup, setPopup}) => {
+    const [trailerUrl, setTrailerUrl] = useState('')
     const opts = {
-        height: '390',
+        height: '350',
         width: '100%',
+        file: {
+            attributes: {
+              crossOrigin: 'anonymous',
+            },
+        },
         playerVars: {
-            autoplay: 1
+            autoplay: 1,
+            origin: window.location.host,  
         }
       }
+
+      useEffect(() => {
+        if(popup !== false) {
+            if(trailerUrl) {
+                setTrailerUrl('')
+            } else {
+                console.log(movieTrailer('Up'))
+                movieTrailer(null ,{ tmdbId: popup.id })
+                .then((url: any) => {
+                    console.log(url)
+                    console.log(popup.id)
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get('v')!)
+                }).catch((err: any) => console.log(err))
+            }
+        }
+      }, [popup])
+
         return (
         <div className='popup_container'>
             <div className='popup'>
-            {/* <YouTube opts={opts}/> */}
+            {trailerUrl && <YouTube opts={opts} videoId={trailerUrl}/>}
             <button className='popup_close' onClick={() => setPopup(false)}>x</button>
                 <div className='popup_content'>
                 <div className='popup_content-tags'>
-                    <h5 className='popup_content-match'>{popup.vote_average * 10}% Match</h5>
+                    <h5 className='popup_content-match'>{Math.round(popup.vote_average * 10)}% Match</h5>
                     <h5 className='popup_content-date'>{popup.first_air_date}</h5>
                     <i className="material-icons popup_content-hd">hd</i>
                     <i className="material-icons popup_content-4k">4k</i>
